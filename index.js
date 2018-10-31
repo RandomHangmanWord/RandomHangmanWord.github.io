4c8e335cb26d06da40b44000b6e0e8adb1e47068
George Beckwith <george.jbeckwith@googlemail.com>
2018-10-31 18:11:32 +0000

    All words added

---
+++ index.js
@@ -0,0 +1,15 @@
+var words = ["ability", "according", "actually", "administration", "agreement", "beautiful", "behavior", 
+"between", "business", "board", "campaign", "candidate", "central", "certainly", "character", "daughter", 
+"democratic", "development", "discussion", "during", "economic", "education", "environmental", "everybody", 
+"experience", "family", "feeling", "fill", "financial", "foreign", "generation", "government", "growth", 
+"green", "glass", "happen", "health", "heavy", "himself", "hospital", "image", "including", "information", 
+"institution", "international", "job", "join", "just", "keep", "key", "kitchen", "knowledge", "know", "language", 
+"lawyer", "leader", "leave", "letter", "magazine", "maintain", "management", "manager", "material", "national", 
+"necessary", "newspaper", "none", "number", "official", "operation", "opportunity", "organization", "outside", 
+"particularly", "pattern", "performance", "personal", "player", "quality", "question", "quickly", "quite", 
+"race", "rather", "reality", "realize", "recognize", "same", "school", "scientist", "season", "seem", "talk", 
+"technology", "television", "themselves", "think", "understand", "until", "upon", "usually", "under", "violence", 
+"vote", "voice", "visit", "view", "wait", "walk", "want", "watch", "weapon", "yard", "year", "young", "yourself"];
+
+var word = words[Math.floor(Math.random()*words.length)];
+document.write (word);
